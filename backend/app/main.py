aecb0d4170a7841557163f08989275c295e45da1
Vikashiu <163726384+Vikashiu@users.noreply.github.com>
2026-06-01 22:31:42 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,7 @@ app = FastAPI(title="Inventory System API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[*],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"], # Allow all methods (GET, POST, PUT, DELETE)
     allow_headers=["*"], # Allow all headers
